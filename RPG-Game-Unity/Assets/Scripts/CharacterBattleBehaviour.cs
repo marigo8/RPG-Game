cb310c595cadee2d2991c0baccc9d7c1e64dd4a3
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-28 23:32:56 -0700

    Made Prefabs, Started work on battles

---
+++ RPG-Game-Unity/Assets/Scripts/CharacterBattleBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterBattleBehaviour : MonoBehaviour
+{
+    public CharacterData character;
+    
+    public Transform idlePoint, attackPoint;
+
+    public Vector3 offset;
+
+    private IEnumerator MoveTo(Transform destination)
+    {
+        var position = destination.position + offset;
+        yield return new WaitUntil(() => Move(position));
+    }
+
+    private bool Move(Vector3 destination)
+    {
+        transform.position = Vector3.Lerp(transform.position, destination, 6f*Time.deltaTime);
+        var distance = Vector3.Distance(transform.position, destination);
+
+        return (distance < .1f);
+    }
+}
--- RPG-Game-Unity/Assets/Scripts/FloatData.cs
+++ RPG-Game-Unity/Assets/Scripts/FloatData.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "Values/Float")]
 public class FloatData : ScriptableObject
