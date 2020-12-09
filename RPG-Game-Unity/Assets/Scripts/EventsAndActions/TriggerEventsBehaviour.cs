b225f80fa110fdd2fdbcd167eb2818bd0329fda3
Mario Tijerino <mariogotijerino@gmail.com>
2020-12-09 10:37:21 -0700

    Made High angle player cam

---
+++ RPG-Game-Unity/Assets/Scripts/EventsAndActions/TriggerEventsBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventsBehaviour : EventsBehaviour
+{
+    public UnityEvent<Collider> triggerEnterEvent, triggerStayEvent, triggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke(other);
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        triggerStayEvent.Invoke(other);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke(other);
+    }
+}
