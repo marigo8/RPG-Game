2eed5dec01d0ecb1a17f7bded6c01ec6ace96659
Mario Tijerino <mariogotijerino@gmail.com>
2020-12-10 23:55:37 -0700

    Camera Moves up and down

---
+++ RPG-Game-Unity/Assets/Scripts/Movement/CameraVerticalBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CameraVerticalBehaviour : MonoBehaviour
+{
+    public IDContainer playerMoveState;
+    
+    public FloatData sensitivity;
+    public float min, max;
+
+    private CinemachineVirtualCamera virtualCamera;
+    private CinemachineOrbitalTransposer orbitalTransposer;
+
+    private void Start()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        orbitalTransposer = virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+    }
+
+    private void LateUpdate()
+    {
+        var yInput = Input.GetAxis("Mouse Y");
+        var offset = orbitalTransposer.m_FollowOffset;
+        
+        offset.y -= yInput * sensitivity.value * Time.deltaTime;
+
+        offset.y = Mathf.Clamp(offset.y, min, max);
+
+        orbitalTransposer.m_FollowOffset = offset;
+    }
+}
