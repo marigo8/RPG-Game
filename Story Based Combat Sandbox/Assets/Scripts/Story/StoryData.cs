14ce557712638c626beb3b221c639d4838c9a441
Mario Tijerino <mariogotijerino@gmail.com>
2020-12-12 14:31:15 -0700

    removed hideName bool from StoryData

--- Story Based Combat Sandbox/Assets/Scripts/Story/StoryData.cs
+++ Story Based Combat Sandbox/Assets/Scripts/Story/StoryData.cs
@@ -19,7 +19,6 @@ public class StoryData : ScriptableObject
     [System.Serializable]
     public struct LineData
     {
-        public bool hideName;
         public UnityEvent lineEvent;
     }
 
