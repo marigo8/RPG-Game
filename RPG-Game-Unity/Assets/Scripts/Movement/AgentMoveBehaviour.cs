539451040f816cf09dab3a442b1861336febd7e0
Mario Tijerino <mariogotijerino@gmail.com>
2020-12-27 12:58:54 -0700

    Improved Agent movement

---
+++ RPG-Game-Unity/Assets/Scripts/Movement/AgentMoveBehaviour.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshObstacle))]
+public class AgentMoveBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private NavMeshObstacle obstacle;
+    private NavMeshPath path;
+
+    public Transform debugObj;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+        obstacle = GetComponent<NavMeshObstacle>();
+        
+        path = new NavMeshPath();
+        
+        MoveToDestination(debugObj);
+    }
+
+    public void MoveToDestination(Transform destinationObj)
+    {
+        StartCoroutine(Move(destinationObj.position));
+    }
+
+    private IEnumerator Move(Vector3 destination)
+    {
+        // Prepare for Path Calculation
+        obstacle.enabled = false; // obstacle carves the navmesh, so it needs to be disabled before calculating the path
+        yield return new WaitUntil(() => true); // wait one frame so that the navmesh can update.
+        
+        // Calculate Path
+        agent.enabled = true;
+        agent.CalculatePath(destination, path);
+        agent.enabled = false;
+
+        // Move to each corner of the path
+        foreach (var corner in path.corners)
+        {
+            yield return new WaitWhile(() => MoveToCorner(corner));
+        }
+        
+        // Re-enable the obstacle
+        obstacle.enabled = true;
+    }
+
+    private bool MoveToCorner(Vector3 corner)
+    {
+        var between = Vector3.MoveTowards(transform.position, corner, agent.speed*Time.deltaTime);
+        
+        if (Vector3.Distance(corner, between) <= 0) return false;
+
+        if (NavMesh.SamplePosition(between, out var hit, 10f, agent.areaMask))
+        {
+            between = hit.position;
+        }
+
+        transform.position = between;
+
+        return true;
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Gizmos.DrawWireSphere(transform.position, agent.stoppingDistance);
+        var i = 0;
+        foreach (var corner in path.corners)
+        {
+            Gizmos.color = i == 0 ? Color.red : Color.yellow;
+
+            Gizmos.DrawWireSphere(corner,.25f);
+            i++;
+        }
+    }
+}
